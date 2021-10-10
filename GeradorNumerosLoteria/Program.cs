fa17ee57ac877fbb85432dab40e23e770d9d9ab3
Diego Araujo <rzrsdiego@outlook.com>
2021-10-10 00:02:51 -0300

    Implementação de algumas melhorias

--- GeradorNumerosLoteria/Program.cs
+++ GeradorNumerosLoteria/Program.cs
@@ -8,29 +8,41 @@ namespace GeradorNumerosLoteria
     {
         static void Main(string[] args)
         {
-            int numeroInicial, numeroFinal, quantidadeRodadas;
-            SolicitarNumeros(out numeroInicial, out numeroFinal, out quantidadeRodadas);
-            GerarNumeroRandomico(numeroInicial, numeroFinal, quantidadeRodadas);
+            int numeroInicial, numeroFinal, quantidadeNumerosPorJogo, quantidadeJogos;
+            SolicitarNumeros(out numeroInicial, out numeroFinal, out quantidadeNumerosPorJogo, out quantidadeJogos);
+            Processar(numeroInicial, numeroFinal, quantidadeNumerosPorJogo, quantidadeJogos);
         }
 
-        private static void SolicitarNumeros(out int numeroInicial, out int numeroFinal, out int quantidadeRodadas)
+        private static void SolicitarNumeros(out int numeroInicial, out int numeroFinal, out int quantidadeNumerosPorJogo, out int quantidadeJogos)
         {
             Console.Write("Entre com o número inicial: ");
             numeroInicial = int.Parse(Console.ReadLine());
             Console.Write("Entre com o número final: ");
             numeroFinal = int.Parse(Console.ReadLine());
-            Console.Write("Informe a quantidade de rodadas: ");
-            quantidadeRodadas = int.Parse(Console.ReadLine());
+            Console.Write("Informe a quantidade de números por jogo: ");
+            quantidadeNumerosPorJogo = int.Parse(Console.ReadLine());
+            Console.Write("Informe a quantidade de jogos: ");
+            quantidadeJogos = int.Parse(Console.ReadLine());
         }
 
-        private static void GerarNumeroRandomico(int numeroInicial, int numeroFinal, int quantidadeRodadas)
+        private static void Processar(int numeroInicial, int numeroFinal, int quantidadeNumerosPorJogo, int quantidadeJogos)
+        {
+            for (int i = 1; i <= quantidadeJogos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Jogo número {i}:");
+                GerarNumeroRandomico(numeroInicial, numeroFinal, quantidadeNumerosPorJogo);
+                Console.WriteLine();
+            }
+        }
+        private static void GerarNumeroRandomico(int numeroInicial, int numeroFinal, int quantidadeNumerosPorJogo)
         {
             var rnd = new Random();
             List<int> numerosGerados = new List<int>();
 
             int numeroGerado;
 
-            for (int i = 0; i < quantidadeRodadas; i++)
+            for (int i = 0; i < quantidadeNumerosPorJogo; i++)
             {
                 numeroGerado = rnd.Next(numeroInicial, numeroFinal + 1);
 
